bceb3eb4f3bdcfdb59661e9c4e65fbf58f575b6c
ivan <ielizalde.dev@gmail.com>
2024-05-14 18:10:01 +0200

    implemented vote system

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var joke = document.getElementById('joke');
-var jokeBtn = document.getElementById('jokeBtn');
-jokeBtn === null || jokeBtn === void 0 ? void 0 : jokeBtn.addEventListener('click', function (e) {
+"use strict";
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+const emojiBtn1 = document.getElementById('emojiBtn1');
+const emojiBtn2 = document.getElementById('emojiBtn2');
+const emojiBtn3 = document.getElementById('emojiBtn3');
+jokeBtn === null || jokeBtn === void 0 ? void 0 : jokeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     generateJoke();
-    console.log('Button clicked');
 });
 function generateJoke() {
     fetch('https://icanhazdadjoke.com/', {
@@ -11,10 +14,40 @@ function generateJoke() {
             'Accept': 'application/json'
         }
     })
-        .then(function (res) { return res.json(); })
-        .then(function (data) {
+        .then((res) => res.json())
+        .then((data) => {
         if (joke) {
             joke.innerHTML = data.joke;
         }
     });
+}
+emojiBtn1 === null || emojiBtn1 === void 0 ? void 0 : emojiBtn1.addEventListener('click', () => vote(1));
+emojiBtn2 === null || emojiBtn2 === void 0 ? void 0 : emojiBtn2.addEventListener('click', () => vote(2));
+emojiBtn3 === null || emojiBtn3 === void 0 ? void 0 : emojiBtn3.addEventListener('click', () => vote(3));
+function vote(score) {
+    let joke = document.getElementById('joke');
+    let emojiBtn = document.getElementById('emojiBtn' + score);
+    let jokeScore = score;
+    if (joke) {
+        const jokeText = joke.innerHTML;
+        if (emojiBtn) {
+            if (emojiBtn.classList.contains('selected')) {
+                emojiBtn.classList.remove('selected');
+                jokeScore = null;
+            }
+            else {
+                for (let i = 1; i <= 3; i++) {
+                    const otherEmojiBtn = document.getElementById(`emojiBtn${i}`);
+                    otherEmojiBtn === null || otherEmojiBtn === void 0 ? void 0 : otherEmojiBtn.classList.remove('selected');
+                }
+                emojiBtn.classList.add('selected');
+            }
+        }
+        const jokeReport = {
+            joke: jokeText,
+            score: jokeScore,
+            date: new Date().toISOString()
+        };
+        console.log(jokeReport);
+    }
 }
--- app.ts
+++ app.ts
@@ -1,11 +1,20 @@
-
 const joke = document.getElementById('joke');
+
 const jokeBtn = document.getElementById('jokeBtn');
+
+const emojiBtn1 = document.getElementById('emojiBtn1');
+const emojiBtn2 = document.getElementById('emojiBtn2');
+const emojiBtn3 = document.getElementById('emojiBtn3');
+
+type JokeReport = {
+  joke: string;
+  score: number;
+  date: string;
+}
 
 jokeBtn?.addEventListener('click', (e) => {
   e.preventDefault();
   generateJoke();
-  console.log('Button clicked');
 }); 
 
 function generateJoke() {
@@ -21,4 +30,38 @@ function generateJoke() {
       
     }
   }) 
+}
+
+emojiBtn1?.addEventListener('click',() => vote(1));
+emojiBtn2?.addEventListener('click',() => vote(2));
+emojiBtn3?.addEventListener('click',() => vote(3));
+
+function vote(score: number | null) {
+  let joke = document.getElementById('joke');
+  let emojiBtn = document.getElementById('emojiBtn' + score);
+  let jokeScore = score;
+
+  if (joke) {
+    const jokeText = joke.innerHTML;
+    
+    if (emojiBtn) {
+      if (emojiBtn.classList.contains('selected')) {
+        emojiBtn.classList.remove('selected');
+        jokeScore = null;    
+      } else {
+        for (let i = 1; i <= 3; i++) {
+          const otherEmojiBtn = document.getElementById(`emojiBtn${i}`);
+          otherEmojiBtn?.classList.remove('selected');
+        }
+        emojiBtn.classList.add('selected');
+      }
+    }    
+     
+    const jokeReport: JokeReport = {
+      joke: jokeText,
+      score: jokeScore as number, 
+      date: new Date().toISOString()
+    }
+    console.log(jokeReport);
+  }
 }
